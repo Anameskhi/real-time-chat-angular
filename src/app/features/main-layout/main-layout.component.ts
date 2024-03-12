ad6234fbf289523792ae34f5049dcf24322578b6
Anameskhi <a.meskhii@gmail.com>
2024-03-12 16:17:23 +0400

    added logout logic

--- src/app/features/main-layout/main-layout.component.ts
+++ src/app/features/main-layout/main-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth-service/auth-service.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-main-layout',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class MainLayoutComponent {
   authorized = false
+  constructor(
+    private router: Router,
+    public snackBar: MatSnackBar
+
+  ){}
+
+  logout() {
+    const token = localStorage.getItem('Token');
+    localStorage.removeItem('Token');
+    localStorage.clear();
+
+    const redirectCallback = () => {
+      this.router.navigate(['']);
+      this.snackBar.open(`Logged out successfully`, 'Close', {
+        duration: 2000, horizontalPosition: 'right', verticalPosition: 'top'
+      });
+    };
+
+    // Check if the token was present before removal
+    if (!token) {
+      redirectCallback();
+    } else {
+      // You might want to add a delay to make sure localStorage operations are completed
+      setTimeout(() => {
+        if (!localStorage.getItem('Token')) {
+          redirectCallback();
+        } else {
+          this.snackBar.open(`User could not be logged out`, 'Close', {
+            duration: 5000, horizontalPosition: 'right', verticalPosition: 'top'
+          });
+        }
+      }, 100);
+    }
+  }
 }
